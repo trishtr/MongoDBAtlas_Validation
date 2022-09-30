ec1ad6d5b2256882e84d0d353ca1f2ce56dad3c8
trishtr <73185951+trishtr@users.noreply.github.com>
2022-09-30 10:20:39 -0800

    Create LogContainer.java

---
+++ src/test/java/models/LogContainer.java
@@ -0,0 +1,26 @@
+package dpautomation.models;
+
+public class LogContainer {
+    private String function;
+    private String timestamp;
+    private String result;
+    public String getFunction() {
+        return function;
+    }
+    public void setFunction(String function) {
+        this.function = function;
+    }
+    public String getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+    public String getResult() {
+        return result;
+    }
+    public void setResult(String result) {
+        this.result = result;
+    }
+    
+}
